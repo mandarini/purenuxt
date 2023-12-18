cebf0553b3e0052a022da0e798c3d80a194b150e
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-12-18 19:50:27 +0200

    updated outputs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  buildDir: 'kater/.nuxt',
+  nitro: {
+    output: {
+      dir: 'kater/.output',
+    },
+  },
 })
